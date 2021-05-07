8a49abe6fcc59fdebf66870e4cf3078234c13035
ATremonte <adamtremonte@gmail.com>
2021-05-07 09:55:08 -0500

    More aggressively reset the state
    More aggressively reset the state, avoid message bombing on login.

--- src/main/java/com/discordlevelnotifications/LevelNotificationsPlugin.java
+++ src/main/java/com/discordlevelnotifications/LevelNotificationsPlugin.java
@@ -4,7 +4,10 @@ import com.google.common.base.Strings;
 import com.google.inject.Provides;
 import lombok.extern.slf4j.Slf4j;
 import net.runelite.api.Client;
+import net.runelite.api.GameState;
+import net.runelite.api.events.GameStateChanged;
 import net.runelite.api.events.GameTick;
+import net.runelite.api.events.UsernameChanged;
 import net.runelite.client.config.ConfigManager;
 import net.runelite.client.eventbus.Subscribe;
 import net.runelite.client.plugins.Plugin;
@@ -55,6 +58,22 @@ public class LevelNotificationsPlugin extends Plugin
 	@Override
 	protected void shutDown() throws Exception
 	{
+
+	}
+
+	@Subscribe
+	public void onUsernameChanged(UsernameChanged usernameChanged)
+	{
+		resetState();
+	}
+
+	@Subscribe
+	public void onGameStateChanged(GameStateChanged gameStateChanged)
+	{
+		if (gameStateChanged.getGameState().equals(GameState.LOGIN_SCREEN))
+		{
+			resetState();
+		}
 	}
 
 	@Subscribe
@@ -83,7 +102,7 @@ public class LevelNotificationsPlugin extends Plugin
 		int level = statChanged.getLevel();
 
 		// .contains wasn't behaving so I went with == null
-		if (currentLevels.get(skillName) == null)
+		if (currentLevels.get(skillName) == null || currentLevels.get(skillName) == 0)
 		{
 			currentLevels.put(skillName, level);
 			return;
@@ -214,5 +233,13 @@ public class LevelNotificationsPlugin extends Plugin
 		ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
 		ImageIO.write(bufferedImage, "png", byteArrayOutputStream);
 		return byteArrayOutputStream.toByteArray();
+	}
+
+	private void resetState()
+	{
+		currentLevels.clear();
+		leveledSkills.clear();
+		shouldSendMessage = false;
+		ticksWaited = 0;
 	}
 }
